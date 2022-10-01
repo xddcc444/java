fd41d611a2170e504add8cae1194584017a2ba03
vykozheurov <100416318+vykozheurov@users.noreply.github.com>
2022-10-01 11:08:13 +0300

    Add files via upload

--- practice2/prct2.java
+++ practice2/prct2.java
@@ -15,31 +15,29 @@ public class prct2 {
 
 
         Dog wolf = new Dog("Grey", 3);
-        wolf.set_age(6);
+        //wolf.set_age(3); //reset age
         System.out.println(wolf);
 
         Dog dogs[] = { new Dog("Black", 6), new Dog("White", 9), new Dog("Purple", 1) };
 
 
-        Dog_kennel dog_kennel = new Dog_kennel();
-        System.out.println(dog_kennel);
+        Dog_nursery dog_nursery = new Dog_nursery();
+        System.out.println(dog_nursery);//create nursery
 
-        dog_kennel.add_dog(wolf);
-        System.out.println(dog_kennel);
+        dog_nursery.add_dog(wolf);
+        System.out.println(dog_nursery);//test adding one dog
 
-        dog_kennel.add_dogs(dogs);
-        System.out.println(dog_kennel);
+        dog_nursery.add_dogs(dogs);
+        System.out.println(dog_nursery);//test adding some dogs
     }
 }
 
 class Shape {
     private String name;
-
     Shape(String name) {
         this.name = name;
-        System.out.println("\n[+] Shape object " + this.name + " was created");
+        System.out.println("\nAdded Shape object " + this.name + " (created)");
     }
-
     public String toString() {
         return "Shape: " + this.name;
     }
@@ -48,84 +46,70 @@ class Shape {
 class Dog {
     private String name;
     private int age;
-
-
     Dog(String name, Integer age) {
         this.name = name;
         this.age = age;
-        System.out.println("\nAdded Dog object with name: " + this.name + " was created");
+        System.out.println("\nAdded Dog object with name: " + this.name + " (created)");
     }
-
     // Age setter
     public void set_age(int age) {
         if (age > 0 && age < 100) {
             this.age = age;
-            System.out.println("Dog age set as: " + this.age);
+            //System.out.println("Dog's age set as: " + this.age);
         }
     }
-
     // Age getter
     public int get_age() {
         return this.age;
     }
-
     // Name setter
     public void set_name(String name) {
         this.name = name;
         System.out.println("Dog name set as: " + this.name);
     }
-
     // Name getter
     public String get_name() {
         return this.name;
     }
 
-    // Dog age as human age getter
     public int get_human_age() {
         return this.age * 7;
     }
 
     public String toString() {
-        return "Dog " + this.name + " with age " + this.age;
+        return "Dog " + get_name() + " with age " + get_age();
     }
 }
 
-class Dog_kennel {
-    private List<Dog> dog_kennel_array = new ArrayList<Dog>();
-
+class Dog_nursery {
+    private List<Dog> dog_nursery_array = new ArrayList<Dog>();
     {
-        System.out.println("\nAdded Dog kennel object was created");
+        System.out.println("\nAdded Dog nursery object (created)");
     }
-
-    // Add one dog
+    // test on one dog
     public void add_dog(Dog dog) {
-        this.dog_kennel_array.add(dog);
-        System.out.println(dog + ". This dog added into the dog kennel successfully");
+        this.dog_nursery_array.add(dog);
+        System.out.println(dog + ". This dog was added into nursery successfully");
     }
-
-    // Add many dogs
+    // test on multiple dogs
     public void add_dogs(Dog dogs[]) {
-        //this.dog_kennel_array = Stream.concat(Arrays.stream(this.dog_kennel_array.toArray()), Arrays.stream(dogs)).toArray(Dog[]::new);
-        this.dog_kennel_array.addAll(new ArrayList<Dog>(Arrays.asList(dogs)));
-        System.out.println("Dogs: " + Arrays.toString(dogs) + " added into the dog kennel successfully");
+        this.dog_nursery_array.addAll(new ArrayList<Dog>(Arrays.asList(dogs)));
+        System.out.println("Dogs: " + Arrays.toString(dogs) + " added into the dog nursery successfully");
     }
-
     public String toString() {
-        if (!this.dog_kennel_array.isEmpty()) return "Dog kennel: " + this.dog_kennel_array;
-        else return "Dog kennel empty!";
+        if (!this.dog_nursery_array.isEmpty()) return "Dog nursery: " + this.dog_nursery_array;
+        else return "Dog nursery empty!";
     }
 }
 
 class Ball {
     int radius;
     int diametr;
-
     Ball(int radius) {
         this.radius = radius;
         this.diametr = this.radius * 2;
-        System.out.println("\nAdded Ball object was created");
+        System.out.println("\nAdded Ball object (created)");
     }
-
     void display_info() {
         System.out.printf("Radius: %s \tDiameter: %d\n", this.radius, this.diametr);
     }
@@ -134,14 +118,12 @@ class Ball {
 class Book {
     String name;
     int pages;
-
     Book(String name, int pages) {
         this.name = name;
         this.pages = pages;
-        System.out.println("\nAdded Book object was created");
+        System.out.println("\nAdded Book object (created)");
     }
-
     void display_info() {
-        System.out.printf("Book name: %s \tPages: %d\n", this.name, this.pages);
+        System.out.printf("Book's name: %s \tPages: %d\n", this.name, this.pages);
     }
 }
