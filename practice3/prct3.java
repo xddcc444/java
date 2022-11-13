8eae3dbe31514d7323cdb17c27bf8622103bb7f5
vykozheurov <100416318+vykozheurov@users.noreply.github.com>
2022-11-13 16:10:13 +0300

    Add files via upload

--- practice3/prct3.java
+++ practice3/prct3.java
@@ -6,37 +6,37 @@ public class prct3 {
 
         // test Circle
         Circle circle = new Circle(7);
-        System.out.println(circle.toString() + ", circle area: " + String.format("%.3f", circle.get_area()));
-        circle.set_radius(32.10);
-        System.out.println(circle.toString() + ", circle area: " + String.format("%.3f", circle.get_area()));
+        System.out.println(circle.toString() + ", circle area: " + String.format("%.3f", circle.getArea()));
+        circle.setRadius(32.10);
+        System.out.println(circle.toString() + ", circle area: " + String.format("%.3f", circle.getArea()));
 
         // test Human
         Human human = new Human("Ivan", 18);
-        human.set_age(22);//re set age
-        human.display_info();
+        human.setAge(22);//re set age
+        human.displayInfo();
 
         // test Human head
-        Human.Head human_head = human.new Head();
-        human_head.think();
-        human_head.sleep(false);
-        human_head.sleep(true);
+        Human.Head humanHead = human.new Head();
+        humanHead.think();
+        humanHead.sleep(false);
+        humanHead.sleep(true);
 
         // test Human hand
-        Human.Hand human_hand = human.new Hand();
-        human_hand.move();
-        human_hand.drop_item();
-        human_hand.hold_item("cup");
-        human_hand.drop_item();
+        Human.Hand humanHand = human.new Hand();
+        humanHand.move();
+        humanHand.dropItem();
+        humanHand.holdItem("cup");
+        humanHand.dropItem();
 
         // test Human leg
-        Human.Leg human_leg = human.new Leg();
-        human_leg.move();
+        Human.Leg humanLeg = human.new Leg();
+        humanLeg.move();
 
         // Book class test
         Book book = new Book("ShortStories", "GreyG", new Date());
         System.out.println(book);
-        book.set_name("1999");
-        book.set_writing_date(new Date(1));
+        book.setName("1999");
+        book.setWritingDate(new Date(1));
         System.out.println(book);
     }
 }
@@ -51,21 +51,21 @@ class Circle {
         System.out.println("Added Circle object (created)");
     }
     // Radius getter
-    double get_radius() {
+    double getRadius() {
         return this.radius;
     }
     // Radius setter
-    void set_radius(double radius) {
+    void setRadius(double radius) {
         this.radius = radius;
         this.diameter = this.radius * 2;
         System.out.println("Added Radius " + this.radius + " (set)");
     }
     // Area getter
-    double get_area() {
+    double getArea() {
         return Math.PI * this.radius * this.radius;
     }
     public String toString() {
-        return "Circle radius: " + get_radius() + ", diameter: " + this.diameter;
+        return "Circle radius: " + getRadius() + ", diameter: " + this.diameter;
     }
 }
 
@@ -79,27 +79,27 @@ class Human {
         System.out.println("Added Human object (created)");
     }
 
-    void display_info() {
+    void displayInfo() {
         System.out.println("Human name: " + this.name + ", age: " + this.age);
     }
 
     // Name getter
-    String get_name() {
+    String getName() {
         return this.name;
     }
 
     // Name setter
-    void set_name(String name) {
+    void setName(String name) {
         this.name = name;
     }
 
     // Age getter
-    int get_age() {
+    int getAge() {
         return this.age;
     }
 
     // Age setter
-    void set_age(int age) {
+    void setAge(int age) {
         if (0 < age && age < 100) {
             this.age = age;
         }
@@ -110,7 +110,7 @@ class Human {
     }
 
     class Head {
-        private boolean is_sleeping = false;
+        private boolean isSleeping = false;
 
         // Head think method
         void think() {
@@ -118,9 +118,9 @@ class Human {
         }
 
         // Head sleep method
-        void sleep(boolean is_sleeping) {
-            this.is_sleeping = is_sleeping;
-            if (this.is_sleeping) System.out.println(name + " now sleeping");
+        void sleep(boolean isSleeping) {
+            this.isSleeping = isSleeping;
+            if (this.isSleeping) System.out.println(name + " now sleeping");
             else System.out.println(name + " now not sleeping");
         }
     }
@@ -134,7 +134,7 @@ class Human {
     }
 
     class Hand {
-        private String item_in_hand;
+        private String itemInHand;
 
         // Hand moving method
         void move() {
@@ -142,16 +142,16 @@ class Human {
         }
 
         // Hand holding item method
-        void hold_item(String item) {
-            this.item_in_hand = item;
-            System.out.println(name + " holds " + this.item_in_hand);
+        void holdItem(String item) {
+            this.itemInHand = item;
+            System.out.println(name + " holds " + this.itemInHand);
         }
 
         // Hand dropping item method
-        void drop_item() {
-            if (item_in_hand != null) {
-                System.out.println(name + " dropped " + this.item_in_hand);
-                this.item_in_hand = null;
+        void dropItem() {
+            if (itemInHand != null) {
+                System.out.println(name + " dropped " + this.itemInHand);
+                this.itemInHand = null;
             }
             else{
                 System.out.println(name + " had nothing to drop");
@@ -163,46 +163,46 @@ class Human {
 class Book {
     private String name;
     private String author;
-    private Date book_writing_date = new Date();
-    Book(String name, String author, Date book_writing_date) {
+    private Date bookWritingDate = new Date();
+    Book(String name, String author, Date bookWritingDate) {
         this.name = name;
         this.author = author;
-        this.book_writing_date = book_writing_date;
+        this.bookWritingDate = bookWritingDate;
         System.out.println("Added Book object (created)");
     }
 
     // Book name getter
-    String get_name() {
+    String getName() {
         return this.name;
     }
 
     // Book name setter
-    void set_name(String name) {
+    void setName(String name) {
         this.name = name;
     }
 
     // Book author getter
-    String get_author() {
+    String getAuthor() {
         return this.author;
     }
 
     // Book author setter
-    void set_author(String author) {
+    void setAuthor(String author) {
         this.author = author;
     }
 
     // Book writing date getter
-    Date get_writing_date() {
-        return this.book_writing_date;
+    Date getWritingDate() {
+        return this.bookWritingDate;
     }
 
     // Book writing date setter
-    void set_writing_date(Date book_writing_date) {
-        this.book_writing_date.setTime(book_writing_date.getTime());
+    void setWritingDate(Date bookWritingDate) {
+        this.bookWritingDate.setTime(bookWritingDate.getTime());
     }
 
     public String toString() {
-        return "Book name: " + get_name() + ", author: " + get_author() + ", writing date: " + get_writing_date();
+        return "Book name: " + getName() + ", author: " + getAuthor() + ", writing date: " + getWritingDate();
     }
 }
 
